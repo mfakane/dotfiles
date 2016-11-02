0ff3879cafec0951e677587258480a31871230f1
MF Akane <star@sorairo.pictures>
2016-11-03 00:22:41 +0900

    Add zsh-autosuggestions

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -3,6 +3,7 @@ zplug "zplug/zplug"
 zplug "~/.zsh", from:local, nice:2, use:"<->_*.zsh"
 
 zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
 zplug "hlissner/zsh-autopair", nice:11
