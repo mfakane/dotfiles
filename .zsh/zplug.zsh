6c1741081256d7341bd22f2a03c6253b1a72d44e
MF Akane <star@sorairo.pictures>
2017-03-19 03:36:01 +0900

    Update nice to defer

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,9 +1,9 @@
 zplug "zplug/zplug"
 
-zplug "~/.zsh", from:local, nice:2, use:"<->_*.zsh"
+zplug "~/.zsh", from:local, defer:1, use:"<->_*.zsh"
 
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search"
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
-zplug "hlissner/zsh-autopair", nice:11
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "hlissner/zsh-autopair", defer:3
