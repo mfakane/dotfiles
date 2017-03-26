282724cf538e31dda1c2dc872ad52c76e6ca97fd
MF Akane <star@sorairo.pictures>
2017-03-27 07:28:19 +0900

    Bind substring search to up and down arrow keys

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -5,5 +5,8 @@ zplug "~/.zsh", from:local, defer:1, use:"<->_*.zsh"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search"
+	bindkey "^[[A" history-substring-search-up
+	bindkey "^[[B" history-substring-search-down
+
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "hlissner/zsh-autopair", defer:3
