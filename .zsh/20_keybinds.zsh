69d0891519d5b29a9750fc7890640ef11567e755
MF Akane <star@sorairo.pictures>
2017-03-28 13:12:34 +0900

    Fix Home/End key not working on WSL

--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -1,5 +1,9 @@
 bindkey '\e[1~' beginning-of-line
+bindkey '\eOH' beginning-of-line
+bindkey '\e[H' beginning-of-line
 bindkey '\e[4~' end-of-line
+bindkey '\eOF' end-of-line
+bindkey '\e[F' end-of-line
 bindkey '\e[2~' overwrite-mode
 bindkey '\e[3~' delete-char
 bindkey '\e[5~' beginning-of-buffer-or-history
