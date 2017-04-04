31ea82c4eb7dbc3fa86995f4add9f9d94c00e1c8
MF Akane <star@sorairo.pictures>
2017-04-04 15:57:23 +0900

    Fix zsh-history-substring-search not working when zgen is loaded from init script

--- .zsh/00_zgen.zsh
+++ .zsh/00_zgen.zsh
@@ -3,12 +3,9 @@ source "${HOME}/.zgen/zgen.zsh"
 if ! zgen saved; then
 	zgen load "zsh-users/zsh-completions"
 	#zgen load "zsh-users/zsh-autosuggestions"
-	zgen load "zsh-users/zsh-history-substring-search"
-		bindkey "^[[A" history-substring-search-up
-		bindkey "^[[B" history-substring-search-down
-
 	zgen load "zsh-users/zsh-syntax-highlighting"
 	zgen load "hlissner/zsh-autopair"
-	
+	zgen load "zsh-users/zsh-history-substring-search"
+
 	zgen save
 fi
--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -4,6 +4,8 @@ bindkey '\e[H' beginning-of-line
 bindkey '\e[4~' end-of-line
 bindkey '\eOF' end-of-line
 bindkey '\e[F' end-of-line
+bindkey '\e[A' history-substring-search-up
+bindkey '\e[B' history-substring-search-down
 bindkey '\e[2~' overwrite-mode
 bindkey '\e[3~' delete-char
 bindkey '\e[5~' beginning-of-buffer-or-history
