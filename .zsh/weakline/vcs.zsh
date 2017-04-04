7548703b96eb0c475a0ba6c38a887f63bd70fac8
MF Akane <star@sorairo.pictures>
2017-04-04 19:10:05 +0900

    Weakline: Add error display to vcs segment

--- .zsh/weakline/vcs.zsh
+++ .zsh/weakline/vcs.zsh
@@ -11,6 +11,7 @@ WEAKLINE_VCS_ICONS=(
 )
 WEAKLINE_VCS_BACKGROUND=green
 WEAKLINE_VCS_CHANGED_BACKGROUND=yellow
+WEAKLINE_VCS_ERROR_BACKGROUND=red
 WEAKLINE_VCS_FOREGROUND=black
 
 autoload -Uz add-zsh-hook
@@ -19,7 +20,13 @@ autoload -Uz vcs_info
 function _weakline_vcs_precmd() { LANG=C vcs_info }
 add-zsh-hook precmd _weakline_vcs_precmd
 
+# Use 3 formats for:
+# $vcs_info_msg_0_ ordinary messages
+# $vcs_info_msg_1_ warning messages
+# $vcs_info_msg_2_ error messages
+zstyle ':vcs_info:*' max-exports 3
 zstyle ':vcs_info:*' formats "${WEAKLINE_VCS_ICONS[BRANCH]} %b" "%u%c"
+zstyle ':vcs_info:*' actionformats "${WEAKLINE_VCS_ICONS[BRANCH]} %b" "%u%c" "%a"
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr " ${WEAKLINE_VCS_ICONS[STAGED]}"
 zstyle ':vcs_info:*' unstagedstr " ${WEAKLINE_VCS_ICONS[UNSTAGED]}"
@@ -54,10 +61,17 @@ function weakline_vcs() {
 	if [[ -z "${vcs_info_msg_0_}" ]]; then return; fi
 	
 	local background=$WEAKLINE_VCS_BACKGROUND
-		
+	local message=$vcs_info_msg_0_
+	
 	if [[ -n "${vcs_info_msg_1_}" ]]; then
 		background=$WEAKLINE_VCS_CHANGED_BACKGROUND
+		message+=$vcs_info_msg_1_
 	fi
 	
-	weakline_write_segment "${vcs_info_msg_0_}${vcs_info_msg_1_}" $background $WEAKLINE_VCS_FOREGROUND
+	if [[ -n "${vcs_info_msg_2_}" ]]; then
+		background=$WEAKLINE_VCS_ERROR_BACKGROUND
+		message+=" $vcs_info_msg_2_"
+	fi
+	
+	weakline_write_segment "$message" $background $WEAKLINE_VCS_FOREGROUND
 }
