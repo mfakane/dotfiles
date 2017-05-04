62d95e3176ce87091d4e6ea183d968c63fe411a6
MF Akane <star@sorairo.pictures>
2017-05-05 00:22:38 +0900

    Change window title to show current directory

---
+++ .zsh/60_title.zsh
@@ -0,0 +1,12 @@
+autoload -Uz add-zsh-hook
+
+case "$TERM" in
+	kterm*|xterm*)
+		function _title_precmd() {
+			local path=$(print -P "%(5~|%-1~/.../%3~|%4~)")
+			echo -ne "\033]0;$USER@${(C)HOST}:$path\007"
+		}
+
+		add-zsh-hook precmd _title_precmd
+		;;
+esac
