a9eb1fe8c20f32ff6150dfbe468af438454c3ced
MF Akane <star@sorairo.pictures>
2017-05-08 03:41:13 +0900

    Add showing current executing command on title

--- .zsh/60_title.zsh
+++ .zsh/60_title.zsh
@@ -2,11 +2,22 @@ autoload -Uz add-zsh-hook
 
 case "$TERM" in
 	kterm*|xterm*|screen*|tmux*)
-		function _title_precmd() {
+		function _title() {
 			local path=$(print -P "%(5~|%-1~/.../%3~|%4~)")
-			echo -ne "\033]0;$USER@${(C)HOST}:$path\007"
+			local userhost="$USER@${(C)HOST}"
+
+			echo -n "$userhost:$path"
+		}
+		
+		function _title_precmd() {
+			echo -ne "\033]0;$(_title)\007"
+		}
+
+		function _title_preexec() {
+			echo -ne "\033]0;$(_title) - $1\007"
 		}
 
 		add-zsh-hook precmd _title_precmd
+		add-zsh-hook preexec _title_preexec
 		;;
 esac
