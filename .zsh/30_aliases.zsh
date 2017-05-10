7f7a92dea89970a81bce51b7373ab8ad58e79b49
MF Akane <star@sorairo.pictures>
2017-05-11 01:48:58 +0900

    Add some aliases for tmux session

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -4,3 +4,17 @@ alias free='free -h'
 alias crontab='crontab -i'
 alias jobs='jobs -l'
 alias ssh='ssh -A'
+
+if [[ -n $TMUX ]]; then
+	function tmux-ssh() {
+		local cmd="ssh -A $@"
+		tmux neww -n "$*" "$cmd"
+	}
+	alias ssh='tmux-ssh'
+	
+	function tmux-nano() {
+		local cmd="nano $@"
+		tmux neww -n "$*" "$cmd"
+	}
+	alias nano='tmux-nano'
+fi
