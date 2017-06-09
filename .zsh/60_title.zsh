832e9e9e05c4d50f5da6e46b3a3b95cd74ba5916
MF Akane <star@sorairo.pictures>
2017-06-09 13:13:00 +0900

    Weakline: Show hostname only up to the first dot for use in FQDN named hosts

--- .zsh/60_title.zsh
+++ .zsh/60_title.zsh
@@ -4,7 +4,7 @@ case "$TERM" in
 	kterm*|xterm*|screen*|tmux*)
 		function _title() {
 			local dir=$(print -P "%(5~|%-1~/.../%3~|%4~)")
-			local userhost="$USER@${(C)HOST}"
+			local userhost="$USER@${(C)HOST/.*}"
 
 			echo -n "$userhost:$dir"
 		}
