9a17d5cc86881ca6e5a3c1a7426123a7494d930b
MF Akane <star@sorairo.pictures>
2021-05-14 22:23:15 +0900

    Updat init script

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -3,6 +3,6 @@
 trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 
-if [[ ! -f ~/.zgen/zgen.zsh ]]; then
-	git clone https://github.com/tarjoilija/zgen ~/.zgen
+if [[ ! -f ~/.zinit/zinit.zsh ]]; then
+	git clone https://github.com/zdharma/zinit.git ~/.zinit
 fi
