0a24ade6dbca1ec7ed6b7f148ed36ef44ad14aa4
MF Akane <star@sorairo.pictures>
2021-06-24 18:39:56 +0900

    Add message for installing packages

--- lib/dotfiles/install.d/30_packages.sh
+++ lib/dotfiles/install.d/30_packages.sh
@@ -6,12 +6,18 @@ type exa &> /dev/null || packages="$packages exa"
 if [ -n "$packages" ]; then
 	if type pacman &> /dev/null; then
 		# Arch Linux
+		echo "Installing packages"
+		echo "> sudo pacman -Sy --noconfirm $packages"
 		sudo pacman -Sy --noconfirm $packages
 	elif type apt &> /dev/null; then
 		# Debian
+		echo "Installing packages"
+		echo "> sudo apt install -y $packages"
 		sudo apt install -y $packages
 	elif type brew &> /dev/null; then
 		# Homebrew
-		sudo brew install $packages
+		echo "Installing packages"
+		echo "> brew install $packages"
+		brew install $packages
 	fi
 fi
