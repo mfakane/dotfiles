5fdce39e469f36004208b5229386542e595d37d7
MF Akane <star@sorairo.pictures>
2021-07-11 06:54:48 +0900

    Add keybindings using terminfo

--- etc/zsh/.zshrc.d/30_bindkey.zsh
+++ etc/zsh/.zshrc.d/30_bindkey.zsh
@@ -1,31 +1,37 @@
 bindkey -d
 
 # Home
-bindkey '^[[H' beginning-of-line
+[[ -n "${terminfo[khome]}" ]] && bindkey "${terminfo[khome]}" beginning-of-line
+bindkey "^[[H" beginning-of-line
 
 # End
-bindkey '^[[F' end-of-line
+[[ -n "${terminfo[kend]}" ]] && bindkey "${terminfo[kend]}" end-of-line
+bindkey "^[[F" end-of-line
 
 # Insert
-bindkey '^[[2~' overwrite-mode
+[[ -n "${terminfo[kich1]}" ]] && bindkey "${terminfo[kich1]}" overwrite-mode
+bindkey "^[[2~" overwrite-mode
 
 # Delete
-bindkey '^[[3~' delete-char
+[[ -n "${terminfo[kdch1]}" ]] && bindkey "${terminfo[kdch1]}" delete-char
+bindkey "^[[3~}" delete-char
 
 # Page Up
-bindkey '^[[5~' beginning-of-buffer-or-history
+[[ -n "${terminfo[kpp]}" ]] && bindkey "${terminfo[kpp]}" beginning-of-buffer-or-history
+bindkey "^[[5~}" beginning-of-buffer-or-history
 
 # Page Down
-bindkey '^[[6~' end-of-buffer-or-history
+[[ -n "${terminfo[knp]}" ]] && bindkey "${terminfo[knp]}" end-of-buffer-or-history
+bindkey "^[[6~}" end-of-buffer-or-history
 
 # Esc
-bindkey '^[' kill-whole-line
+bindkey "^[" kill-whole-line
 
 # Take 10ms for key sequences
 KEYTIMEOUT=1
 
-bindkey '^R' history-incremental-search-backward
-bindkey '^S' history-incremental-search-forward
+bindkey "^R" history-incremental-search-backward
+bindkey "^S" history-incremental-search-forward
 
 # Disable C-s, C-q
 setopt no_flow_control
