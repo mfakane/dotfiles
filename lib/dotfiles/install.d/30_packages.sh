44e796a2da6f8d78c4c2e10d40e476fd6c177ca1
MF Akane <star@sorairo.pictures>
2021-07-30 17:44:58 +0900

    Supress package installation errors

--- lib/dotfiles/install.d/30_packages.sh
+++ lib/dotfiles/install.d/30_packages.sh
@@ -8,16 +8,16 @@ if [ -n "$packages" ]; then
 		# Arch Linux
 		echo "Installing packages"
 		echo "> sudo pacman -Sy --noconfirm $packages"
-		sudo pacman -Sy --noconfirm $packages
+		sudo pacman -Sy --noconfirm $packages 2> /dev/null || true
 	elif type apt-get > /dev/null 2>&1; then
 		# Debian
 		echo "Installing packages"
 		echo "> sudo apt-get install -y $packages"
-		sudo apt-get install -y $packages
+		sudo apt-get install -y $packages 2> /dev/null || true
 	elif type brew > /dev/null 2>&1; then
 		# Homebrew
 		echo "Installing packages"
 		echo "> brew install $packages"
-		brew install $packages
+		brew install $packages 2> /dev/null || true
 	fi
 fi
